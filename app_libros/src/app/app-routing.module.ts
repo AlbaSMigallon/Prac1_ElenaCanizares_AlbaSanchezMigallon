019658dfd0df0f15b42651121e02baab0d3fed1a
elena <ecanizaresj99@gmail.com>
2023-10-04 08:43:40 +0200

    Copia de trabajo actualizada

--- app_libros/src/app/app-routing.module.ts
+++ app_libros/src/app/app-routing.module.ts
@@ -4,8 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
+    redirectTo:'inicio',
+    pathMatch:'full',
+   
+  },
+  {
     path: 'inicio',
     loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
   },
