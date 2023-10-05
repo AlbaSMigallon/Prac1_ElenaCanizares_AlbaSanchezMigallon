d9d4737812c3aafe9f43102cd1cf0ade3a0d6d95
elena <ecanizaresj99@gmail.com>
2023-10-05 13:06:32 +0200

    Arreglo tabs

--- app_libros/src/app/tabs/tabs-routing.module.ts
+++ app_libros/src/app/tabs/tabs-routing.module.ts
@@ -4,8 +4,7 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
-    path: 'tabs',
-          redirectTo: 'tab1',
+    path: '',
     component: TabsPage,
     children: [
       {
@@ -21,16 +20,11 @@ const routes: Routes = [
         path: 'tab3',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
-      {
-        path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
-      }
     ]
   },
   {
-    path: '',
-    redirectTo: '/tabs/tab1',
+    path: 'tabs',
+    redirectTo: '/tabs',
     pathMatch: 'full'
   }
 ];
