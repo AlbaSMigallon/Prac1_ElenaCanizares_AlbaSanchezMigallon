e899c72881f3cc15d457970c49ad13725ea9832b
AlbaSMigallon <albasmigallon@gmail.com>
2023-10-09 14:27:27 +0200

    Ultimas modificaciones de datos

--- app_libros/src/app/datos/datos.page.ts
+++ app_libros/src/app/datos/datos.page.ts
@@ -7,8 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatosPage implements OnInit {
   PC="desktop-outline";
-  logo_libro="./src/assets/icon/libro.png";
-  
+ 
   constructor() { }
 
   ngOnInit() {
