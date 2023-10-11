fa54c803a00181c9bceadc305383a1405607813d
elena <ecanizaresj99@gmail.com>
2023-10-11 18:45:51 +0200

    Cambios de ultima hora al repasar

--- app_libros/src/app/tab2/tab2.page.ts
+++ app_libros/src/app/tab2/tab2.page.ts
@@ -1,3 +1,4 @@
+// tab2.page.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  books: { [key: string]: boolean } = {
+    frankenstein: false,
+    it: false,
+    carterville: false,
+    cuestaNoche: false,
+    faseRubi: false, // Cambiado de faseRubí a faseRubi
+    casaCerebro: false,
+    pozoPendulo: false, // Cambiado de pozoPéndulo a pozoPendulo
+    cthulhu: false,
+    dracula: false,
+  };
 
   constructor() {}
 
+  onBookCheckedChange(bookKey: string) {
+    console.log(`${bookKey} Checked: ${this.books[bookKey]}`);
+  }
+
+  getBookIcon(bookKey: string) {
+    // Lógica para obtener el icono según el libro (debes definirla)
+    // Ejemplo simple:
+    return "flash-outline";
+  }
+
+  getBookTitle(bookKey: string) {
+    // Lógica para obtener el título según el libro (debes definirla)
+    // Ejemplo simple:
+    return "Título del Libro";
+  }
 }
